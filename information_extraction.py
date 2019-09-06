e0d4c3205a522fecb2f23b9c16bcf3e40f02d952
git-ekeh <53243806+git-ekeh@users.noreply.github.com>
2019-09-06 15:13:37 -0400

    Add files via upload

---
+++ information_extraction.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env/python3
+
+
+
+from bs4 import BeautifulSoup
+import requests as rs 
+
+
+def data_scrape():
+    webpage_response = rs.get("https://www.toronto.ca/city-government/data-research-maps/open-data/open-data/open-data-catalogue/#5db8d9e0-1ef4-8bc9-5932-edf87a551163")
+    webpage = webpage_response.content
+    soup = BeautifulSoup(webpage, 'html.parser')
+    page_links = soup.find_all("p")
+    urls = []
+    train = open("training_file.txt", "w+")
+    for tag in page_links: 
+        train.write(str(tag.get_text()))
+    train.close()
+
+data_scrape()
+
